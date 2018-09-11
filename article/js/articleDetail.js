5d9867c733c296fc3c45e434894c868c8a48f21f
ttbf <juquansheng@hotmail.com>
2018-09-11 18:36:18 +0800

    编辑按钮

--- article/js/articleDetail.js
+++ article/js/articleDetail.js
@@ -34,3 +34,36 @@ function articleDetail(){
 	})
 }
 articleDetail();
+
+function toeEdit(){
+        window.location.href = "/yuuki/article/html/editArticle.html";
+    }
+
+function isbelong(){
+        $.ajax({
+            url:"http://localhost:8927/yuuki/article/isbelong?"+articleId,
+            type:"get", 
+            dataType:"json",
+            contentType:"application/json",
+            headers: {  
+            'Accept': 'application/json',  
+            'Content-Type': 'application/json'  
+            },  
+            xhrFields: {
+            withCredentials: true
+            },
+            crossDomain: true,
+            success:function(datas){
+                console.log(datas.message);
+                if (datas.status == 200) {
+                    $(".isbelong").append("<button class='edit' onclick='toeEdit()'>编辑</button>");
+                }else{
+                    $(".isbelong").append("<button class='edit' onclick='toeEdit()'>编辑</button>");
+                }
+            },
+            error:function(datas){
+                console.log("error")
+            }
+        })
+    }
+isbelong();
