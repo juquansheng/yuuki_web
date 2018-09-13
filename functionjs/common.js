4b303249e063bb72a8d7681ee1bf969bdd3dd2be
ttbf <juquansheng@hotmail.com>
2018-09-13 19:47:08 +0800

    第一版完成

--- functionjs/common.js
+++ functionjs/common.js
@@ -1,2 +1,2 @@
-//var global = "http://ttbf.malaxiaoyugan.com/yuuki/"
-var globalUrl = "http://localhost:8927/yuuki"
+var globalUrl = "http://ttbf.malaxiaoyugan.com/yuuki"
+//var globalUrl = "http://localhost:8927/yuuki"
