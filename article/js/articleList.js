3019b593baf2f6ca497a0876801bed032afc38b6
ttbf <juquansheng@hotmail.com>
2018-09-25 10:14:28 +0800

    分页完成

--- article/js/articleList.js
+++ article/js/articleList.js
@@ -1,13 +1,13 @@
 var setTotalCount = 0;
 var settotalPages = 1;
-var page = 1;
-var rows = 10;
+var setpage = 1;
+var setrows = 15;
 
 
 function articleList(){
 	$.ajax({
 		type: "POST",
-		url: window.globalUrl+"article/getlist?page="+page+"&rows="+rows,
+		url: window.globalUrl+"article/getlist?page="+setpage+"&rows="+setrows,
         data: JSON.stringify({
             "userId":null
         }),
@@ -18,9 +18,84 @@ function articleList(){
             },
         crossDomain: true,
         success: function(data){
+            console.log(data);
             $(".article-list").html("");
             console.log(data.data.pageDatas);
+            settotalPages = data.data.totalPages;
+            setTotalCount = data.data.total;
             if (data.status == 200) {
+
+                
+
+                console.log("settotalPages1="+settotalPages);
+                $.each(data.data.pageDatas, function(i, item) {
+                $(".article-list").append("<p class='row-article'>"
+            
+                        +"<span class='article-title'><a href='/yuuki/article/html/articleDetail.html?id="+item.id+"'>"+item.title+"</a>&ensp;&ensp;&ensp;</span>"
+                        +"<span class='createTime'>"+"发布时间："+item.createTimeString+"&ensp;&ensp;&ensp;</span>"
+                        +"<span class='nickName'>"+"作者："+item.nickName+"</span>"
+                        +"</p>");
+                });
+                pageDatas();
+            }else if (data.status == 401) {
+                window.location.href = "login.html"
+            }else{
+                console.log(data.message);
+            }
+        },
+        error: function(jqXHR){
+            alert("Error：" + jqXHR.status);
+        },
+	})
+}
+articleList();
+
+function pageDatas(){
+    console.log("settotalPages2="+settotalPages);
+    $(document).ready(function(){
+        $('#box').paging({
+            initPageNo: 1, // 初始页码
+            totalPages: settotalPages, //总页数
+
+            totalCount: '合计' + setTotalCount + '条数据', // 条目总数
+            slideSpeed: 600, // 缓动速度。单位毫秒
+            jump: true, //是否支持跳转
+            callback: function(page) { // 回调函数
+                console.log("page="+page);
+                setpage = page;
+                
+              updateArticleList();
+ 
+            }
+
+        })
+    })
+}
+
+function updateArticleList(){
+    $.ajax({
+        type: "POST",
+        url: window.globalUrl+"article/getlist?page="+setpage+"&rows="+setrows,
+        data: JSON.stringify({
+            "userId":null
+        }),
+        contentType: 'application/json; charset=UTF-8',
+        dataType: 'json',
+        xhrFields: {
+            withCredentials: true
+            },
+        crossDomain: true,
+        success: function(data){
+            console.log(data);
+            $(".article-list").html("");
+            console.log(data.data.pageDatas);
+            settotalPages = data.data.totalPages;
+            setTotalCount = data.data.total;
+            if (data.status == 200) {
+
+                
+
+                console.log("settotalPages1="+settotalPages);
                 $.each(data.data.pageDatas, function(i, item) {
                 $(".article-list").append("<p class='row-article'>"
             
@@ -38,6 +113,5 @@ function articleList(){
         error: function(jqXHR){
             alert("Error：" + jqXHR.status);
         },
-	})
+    })
 }
-articleList();
